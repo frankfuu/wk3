9a435f40f7a2eb5cbb2d95074e96e0f256380a84
Frank Fu <frankfu1122@gmail.com>
2022-08-15 20:25:08 +1000

    initial

---
+++ app/src/main/java/comp5216/sydney/edu/au/todolist/ToDoItemDao.java
@@ -0,0 +1,25 @@
+package comp5216.sydney.edu.au.todolist;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+import comp5216.sydney.edu.au.todolist.ToDoItem;
+
+@Dao
+public interface ToDoItemDao {
+    @Query("SELECT * FROM todolist")
+    List<ToDoItem> listAll();
+
+    @Insert
+    void insert(ToDoItem toDoItem);
+
+    @Insert
+    void insertAll(ToDoItem... toDoItems);
+
+    @Query("DELETE FROM todolist")
+    void deleteAll();
+}
